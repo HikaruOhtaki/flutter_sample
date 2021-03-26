a9bc8d325b433275c48a1446f05cc3a7e1db40f2
Hikaru.Ohtaki <hikaru.11.oth@icloud.com>
2021-03-26 14:01:54 +0900

    fix: firebase project

--- lib/app.dart
+++ lib/app.dart
@@ -58,6 +58,6 @@ class Authentication extends HookWidget {
     } else if (status == const UserInfoStatus.authenticated()) {
       return TodoListPage();
     }
-    return Container(child: Text('text'));
+    return Container(child: const Text('text'));
   }
 }
